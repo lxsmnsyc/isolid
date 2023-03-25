38518d5f5fbbe687bb22b9b8e32148f4820cf61a
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-25 14:13:30 +0800

    Add island processing

--- packages/isolid/client/index.ts
+++ packages/isolid/client/index.ts
@@ -4,23 +4,28 @@ import {
   createMemo,
   createResource,
   JSX,
+  mergeProps,
   splitProps,
 } from 'solid-js';
+import { hydrate, render } from 'solid-js/web';
 import { CLIENT_PROPS, SERVER_PROPS } from '../shared/constants';
 import {
   ClientComponent,
+  ClientHydrationStrategy,
   ClientProps,
   SerializableProps,
   ServerComponent,
   ServerOptions,
   ServerProps,
 } from '../shared/types';
+import { getFragment, getRoot } from './nodes';
+import processScript from './process-script';
 
 let SCOPE: AsyncServerValue[];
 
-function runWithScope<T>(scope: () => AsyncServerValue[], callback: () => T): T {
+function runWithScope<T>(scope: AsyncServerValue[], callback: () => T): T {
   const parent = SCOPE;
-  SCOPE = scope();
+  SCOPE = scope;
   try {
     return callback();
   } finally {
@@ -90,14 +95,98 @@ export function $$server<P extends SerializableProps>(
 export function $$client<P extends SerializableProps>(
   id: string,
   OriginalComp: ClientComponent<P>,
+  lexicalScope: () => AsyncServerValue[],
 ): ClientComponent<ClientProps<P>> {
   return function Comp(props: ClientProps<P>): JSX.Element {
     const [, rest] = splitProps(props, CLIENT_PROPS);
+    return runWithScope(lexicalScope(), () => createComponent(OriginalComp, rest as P));
+  };
+}
 
-    createMemo(() => id, {
-      name: id,
+async function renderRoot(
+  strategy: ClientHydrationStrategy,
+  marker: Element,
+  renderCallback: () => void,
+): Promise<(() => void) | undefined> {
+  if (strategy['client:load']) {
+    const call = await import('isolid/scheduler/load') as typeof import('../scheduler/on-load');
+    return call.default(renderCallback);
+  }
+  if (strategy['client:visible']) {
+    const call = await import('isolid/scheduler/visible') as typeof import('../scheduler/on-visible');
+    return call.default(marker, renderCallback, true);
+  }
+  if (strategy['client:media'] != null) {
+    const call = await import('isolid/scheduler/media') as typeof import('../scheduler/on-media');
+    return call.default(strategy['client:media'], renderCallback);
+  }
+  if (strategy['client:idle']) {
+    const call = await import('isolid/scheduler/idle') as typeof import('../scheduler/on-idle');
+    return call.default(renderCallback);
+  }
+  if (strategy['client:animation-frame']) {
+    const call = await import('isolid/scheduler/animation-frame') as typeof import('../scheduler/on-animation-frame');
+    return call.default(renderCallback);
+  }
+  if (strategy['client:delay'] != null) {
+    const call = await import('isolid/scheduler/delay') as typeof import('../scheduler/on-delay');
+    return call.default(strategy['client:delay'], renderCallback);
+  }
+  if (strategy['client:interaction']) {
+    const call = await import('isolid/scheduler/interaction') as typeof import('../scheduler/on-interaction');
+    return call.default(strategy['client:interaction'], marker, renderCallback);
+  }
+  if (strategy['client:ready-state']) {
+    const call = await import('isolid/scheduler/ready-state') as typeof import('../scheduler/on-ready-state');
+    return call.default(strategy['client:ready-state'], renderCallback);
+  }
+  return undefined;
+}
+
+type Island<P extends SerializableProps> = (
+  id: string,
+  hasChildren: boolean,
+  props: P,
+  strategy: ClientHydrationStrategy,
+  scope: AsyncServerValue[],
+) => Promise<void>;
+
+export function $$island<P extends SerializableProps>(
+  source: () => Promise<{ default: ClientComponent<P>}>,
+): Island<P> {
+  return async (id, hasChildren, props, strategy, scope) => {
+    const marker = getRoot(id);
+    const Comp = (await source()).default;
+
+    await renderRoot(strategy, marker, () => {
+      const root = () => {
+        if (hasChildren) {
+          const fragment = getFragment(id);
+          return runWithScope(scope, () => (
+            createComponent(Comp, mergeProps(props, {
+              get children() {
+                if (fragment) {
+                  const node = fragment.content.firstChild;
+                  if (node) {
+                    processScript(node);
+                    return node;
+                  }
+                }
+                return null;
+              },
+            }) as P & { children?: JSX.Element })
+          ));
+        }
+        return runWithScope(scope, () => createComponent(Comp, props));
+      };
+
+      if (strategy['client:only']) {
+        render(root, marker);
+      } else {
+        hydrate(root, marker, {
+          renderId: id,
+        });
+      }
     });
-
-    return createComponent(OriginalComp, rest as P);
   };
 }
